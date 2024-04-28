a67a029534db5028729a19394d3dd66152127a2e
ilya <a>
2024-04-28 15:56:56 +0300

    testEquationSolver finished

--- math_solver.cpp
+++ math_solver.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 #include <fstream>
 #include <filesystem>
+#include <limits>
+#include <cassert>
 
 using namespace std;
 
@@ -22,7 +24,7 @@ class InstructionsExporterImpl : public InstructionsExporter {
         string generate_file_name() {
             time_t current_time = time({});
             char current_time_string[size("yyyy_mm_ddThh_mm_ss")];
-            strftime(data(current_time_string), size(current_time_string), "%Y_%m_%dT%H%M%S", gmtime(&current_time));
+            strftime(data(current_time_string), size(current_time_string), "%Y_%m_%.3fT%H%M%S", gmtime(&current_time));
 
             return string(current_time_string) + ".txt";
         }
@@ -70,10 +72,10 @@ class InstructionsExporterImpl : public InstructionsExporter {
 
 class EquationSolver {
     private:
-        int a, b, c;
+        float a, b, c;
         InstructionsExporter* exporter;
     public:
-        EquationSolver(int a, int b, int c, InstructionsExporter* exporter) {
+        EquationSolver(float a, float b, float c, InstructionsExporter* exporter) {
             this->a = a;
             this->b = b;
             this->c = c;
@@ -81,33 +83,56 @@ class EquationSolver {
         }
 
         map<string, float> solve() {
-            char buff[100];
+            char equation_string[100];
             if (a == 0 && b == 0 && c == 0) {
-                sprintf(buff, "Your equation is: 0 = 0");
+                sprintf(equation_string, "Your equation is: 0 = 0");
             } else if (a == 0 && b == 0) {
-                sprintf(buff, "Your equation is: %d = 0", c);
+                sprintf(equation_string, "Your equation is: %.3f = 0", c);
             } else if (a == 0 && c == 0) {
-                sprintf(buff, "Your equation is: %dx = 0", b);
+                sprintf(equation_string, "Your equation is: %.3fx = 0", b);
             } else if (b == 0 && c == 0) {
-                sprintf(buff, "Your equation is: %dx² = 0", a);
+                sprintf(equation_string, "Your equation is: %.3fx² = 0", a);
             } else if (a == 0) {
-                sprintf(buff, "Your equation is: %dx + %d = 0", b, c);
+                sprintf(equation_string, "Your equation is: %.3fx + %.3f = 0", b, c);
             } else if (b == 0) {
-                sprintf(buff, "Your equation is: %dx² + %d = 0", a, c);
+                sprintf(equation_string, "Your equation is: %.3fx² + %.3f = 0", a, c);
             } else if (c == 0) {
-                sprintf(buff, "Your equation is: %dx² + %dx = 0", a, b);
+                sprintf(equation_string, "Your equation is: %.3fx² + %.3fx = 0", a, b);
             } else  {
-                sprintf(buff, "Your equation is: %dx² + %dx + %d = 0", a, b, c);
+                sprintf(equation_string, "Your equation is: %.3fx² + %.3fx + %.3f = 0", a, b, c);
             }
 
-            cout << buff << endl;
-            exporter -> write_text_to_temp_file(buff);
+            cout << equation_string << endl;
+            exporter -> write_text_to_temp_file(equation_string);
+
+            map<string, float> solutions;
+
+            if (a == 0 && b == 0 && c == 0) {
+                solutions["solutions_count"] = numeric_limits<float>::infinity();
+                cout << "There are infinity solutions" << endl;
+
+                return solutions;
+            } else if (a == 0) {
+                if (b == 0) {
+                    solutions["solutions_count"] = 0;
+                    cout << "There are no real solutions" << endl;
+
+                    return solutions;
+                }
+                
+                float x = -c / b;
+
+                solutions["solutions_count"] = 1;
+                solutions["x"] = x;
+
+                return solutions;
+            }
             
 
-            int D = pow(b, 2) - 4 * a * c;
+            float D = pow(b, 2) - 4 * a * c;
 
             char calculating_D_step_string[100];
-            sprintf(calculating_D_step_string, "Calculating Discriminant by D = b² - 4ac:\nD = (%d)² - 4 * (%d) * (%d) = %d", b, a, c, D);
+            sprintf(calculating_D_step_string, "Calculating Discriminant by D = b² - 4ac:\nD = (%.3f)² - 4 * (%.3f) * (%.3f) = %.3f", b, a, c, D);
 
             cout << calculating_D_step_string << endl;
             exporter -> write_text_to_temp_file(calculating_D_step_string);
@@ -115,7 +140,6 @@ class EquationSolver {
             string D_analysis_step_string;
             char calculation_solutions_step_string[300];
 
-            map<string, float> solutions;
             if (D > 0) {
                 D_analysis_step_string = "The D of the equation is greater than 0. There are two solutions\n";
                 
@@ -128,8 +152,8 @@ class EquationSolver {
                 solutions["x1"] = x1;
                 solutions["x2"] = x2;
 
-                sprintf(calculation_solutions_step_string, "Calculating one solution by x = (-b + √D) / (2a):\nx = -(%d + √%d) / (2 * (%d)) = %d\n\n \
-                Calculating another solution by x = (-b - √D) / (2a):\nx = -(%d - √%d) / (2 * (%d)) = %d", b, D, a, x1, b, D, a, x2);
+                sprintf(calculation_solutions_step_string, "Calculating one solution by x = (-b + √D) / (2a):\nx = -(%.3f + √%.3f) / (2 * (%.3f)) = %.3f\n\n \
+                Calculating another solution by x = (-b - √D) / (2a):\nx = -(%.3f - √%.3f) / (2 * (%.3f)) = %.3f", b, D, a, x1, b, D, a, x2);
 
             } else if (D == 0) {
                 D_analysis_step_string = "The D of the equation equals 0. There is one solution\n";
@@ -139,7 +163,7 @@ class EquationSolver {
                 solutions["solutions_count"] = 1;
                 solutions["x"] = x;
 
-                sprintf(calculation_solutions_step_string, "Calculating that one solution by x = (-b) / (2a):\nx = -(%d) / (2 * (%d))", b, a);
+                sprintf(calculation_solutions_step_string, "Calculating that one solution by x = (-b) / (2a):\nx = -(%.3f) / (2 * (%.3f))", b, a);
 
             } else {
                 D_analysis_step_string = "The D of the equation is lower than 0. There are no real solutions\n";
@@ -164,18 +188,84 @@ class EquationSolver {
 
 
 
+class MockInstructionsExporterImpl : public InstructionsExporter {
+    public:
+        void write_text_to_temp_file(string text) override {
+            cout << "write_text_to_temp_file with '" << text << "' called" << endl;
+        }
+
+        void export_instructions() override {}
+};
+
+
+void testEquationSolver() {
+    MockInstructionsExporterImpl* mockExporter = new MockInstructionsExporterImpl();
+    
+    EquationSolver* solver;
+    map<string, float> solutions;
+
+    const string k = "solutions_count";
+    const string x = "x";
+    const string x1 = "x1";
+    const string x2 = "x2";
+    
+
+    solver = new EquationSolver(0, 0, 0, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == numeric_limits<float>::infinity());
+
+    solver = new EquationSolver(0, 0, 5, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 0);
+
+    solver = new EquationSolver(0, 5, 0, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 1 && solutions[x] == 0);
+
+    solver = new EquationSolver(0, 5, 10, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 1 && solutions[x] == -2);
+
+    solver = new EquationSolver(0, 10, 5, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 1 && solutions[x] == -0.5);
+
+    solver = new EquationSolver(4, 0, 0, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 1 && solutions[x] == 0);
+
+    solver = new EquationSolver(10, 0, -100000, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 2 && solutions[x1] == 100 && solutions[x2] == -100);
+
+    solver = new EquationSolver(10, 0, 100000, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 0);
+
+    solver = new EquationSolver(1, -2, 1, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 1 && solutions[x] == 1);
+
+    solver = new EquationSolver(1, -5.5, 2.5, mockExporter);
+    solutions = solver->solve();
+    assert(solutions[k] == 2 && solutions[x1] == 5 && solutions[x2] == 0.5);
+}
+
+
+
 int main()
 {
-    int a, b, c;
+    testEquationSolver();
+    float a, b, c;
 
-    cout << "Enter the coefficients of the equation in format: a b c: ";
-    cin >> a >> b >> c;
+    // cout << "Enter the coefficients of the equation in format: a b c: ";
+    // cin >> a >> b >> c;
 
 
-    InstructionsExporterImpl* exporter = new InstructionsExporterImpl();
-    EquationSolver solver(a, b, c, exporter);
+    // InstructionsExporterImpl* exporter = new InstructionsExporterImpl();
+    // EquationSolver solver(a, b, c, exporter);
 
-    map<string, float> solutions = solver.solve();
-    cout << solutions["solutions_count"]  << solutions["x1"]  << solutions["x2"] << endl;
-    solver.export_instructions();
+    // map<string, float> solutions = solver.solve();
+    // cout << solutions["solutions_count"]  << solutions["x1"]  << solutions["x2"] << endl;
+    // solver.export_instructions();
 }
